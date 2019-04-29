db22e70875645492d6ec1095bf6495278ccce11f
Raditha Dissanayake <raditha@raditha.com>
2019-04-29 07:28:31 +0530

    Still stuck on waiting for the container (pun intended)

--- scripts/wait.sh
+++
@@ -1,8 +0,0 @@
-# Wait for the port to be available
-
-MYSQL_CONTAINER=`docker ps --format '{{.Names}}' --filter name=mysql`
-
-until nc -z $(sudo docker inspect --format='{{.NetworkSettings.IPAddress}}' $MYSQL_CONTAINER) 3306
-do
-    sleep 0.5
-done
