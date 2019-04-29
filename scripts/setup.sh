4efd4d1ab1dc8a474472c1c012e83e26392f6f41
Raditha Dissanayake <raditha@raditha.com>
2019-04-29 21:26:51 +0530

    Abandoning localhost

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,4 +3,8 @@ sudo killall httpd
 sudo killall nginx
 sudo /etc/init.d/mysql stop
 sudo docker-compose up --build -d
-until nc -z localhost 80; do sleep 5; echo '.';  done
+sudo docker ps
+IP=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dockerphp_nginx_1)
+until nc -z $IP 80; do sleep 5; echo '.';  done
+echo $IP
+phpunit scripts/test.php $IP
