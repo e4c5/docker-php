043589336b8d501e17a5b893920c83863889736a
Raditha Dissanayake <raditha@raditha.com>
2019-04-29 08:51:27 +0530

    Waiting for the mysql server to finish installing is killing me

--- test.php
+++ test.php
@@ -3,8 +3,13 @@ use PHPUnit\Framework\TestCase;
  
 class FetchTests extends TestCase {
 	public function test_url() {
-    	$data = file_get_contents("http://localhost/demo.php");
-    	
+		for($i = 0 ; $i < 12 ; $i++) {
+    	    $data = file_get_contents("http://localhost/demo.php");
+    	    if(strstr($data, "prof")) {
+                break;
+            }
+            sleep(10);
+        }
     	$this->assertContains('prof', $data);
   	}
 }
