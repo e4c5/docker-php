bf59ba27377fd19af487a849c1a88b6da995cd38
Raditha Dissanayake <raditha.dissanayake@gmail.com>
2019-04-29 15:25:53 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,14 +3,14 @@ use PHPUnit\Framework\TestCase;
  
 class FetchTests extends TestCase {
 	public function test_url() {
-		for($i = 0 ; $i < 12 ; $i++) {
-    	    $data = file_get_contents("http://localhost/demo.php");
-    	    if(strstr($data, "prof")) {
-                break;
-            }
-            sleep(10);
-        }
-    	$this->assertContains('prof', $data);
+	    for($i = 0 ; $i < 12 ; $i++) {
+    	        $data = file_get_contents("http://localhost/demo.php");
+    	        if(strstr($data, "prof")) {
+                    break;
+                }
+                sleep(10);
+             }
+    	     $this->assertContains('prof', $data);
   	}
 }
 ?>
