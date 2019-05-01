f398c1fad48501ea7e6b431c9cfa3af993fe3083
Raditha Dissanayake <raditha@raditha.com>
2019-05-01 07:55:55 +0530

    updated setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,9 +4,9 @@ sudo killall nginx
 sudo /etc/init.d/mysql stop
 sudo docker-compose up --build -d
 sudo docker ps
-IP=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dockerphp_nginx_1)
-echo $IP
-sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dockerphp_nginx_1
-until nc -z $IP 80; do sleep 5; echo '.';  done
+#IP=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dockerphp_nginx_1)
+#echo $IP
+#sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dockerphp_nginx_1
+until nc -z localhost 80; do sleep 5; echo '.';  done
 
-phpunit scripts/test.php $IP
+phpunit scripts/test.php localhost
