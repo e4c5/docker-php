92237e26b77391759e1d46e4e32f904115287c87
Raditha Dissanayake <raditha@raditha.com>
2019-05-01 08:05:15 +0530

    updated setup.sh

--- scripts/test.php
+++ scripts/test.php
@@ -3,9 +3,9 @@ use PHPUnit\Framework\TestCase;
  
 class FetchTests extends TestCase {
 	public function test_url() {
-        $IP = $_SERVER['argv'][1];
+   
 	    for($i = 0 ; $i < 12 ; $i++) {
-    	        $data = file_get_contents("http://{$IP}/demo.php");
+    	        $data = file_get_contents("http://localhost/demo.php");
     	        if(strstr($data, "prof")) {
                     break;
                 }
