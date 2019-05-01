780b1ed7cb94d554036f3e9e40f1007fe4dabf8c
Raditha Dissanayake <raditha@raditha.com>
2019-05-01 08:01:22 +0530

    updated setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,11 @@ sudo killall httpd
 sudo killall nginx
 sudo /etc/init.d/mysql stop
 sudo docker-compose up --build -d
+sleep 9
 sudo docker ps
 #IP=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dockerphp_nginx_1)
 #echo $IP
 #sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dockerphp_nginx_1
-until nc -z localhost 80; do sleep 5; echo '.';  done
-
+#until nc -z localhost 80; do sleep 5; echo '.';  done
+sleep 9
 phpunit scripts/test.php localhost
